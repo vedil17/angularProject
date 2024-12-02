dd3058916464c9849d6226d3db62e6615fb05278
adil alouan <62723653+vedil17@users.noreply.github.com>
2024-12-02 16:42:42 +0100

    DataBinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RolesComponent } from './components/roles/roles.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RolesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
-
+  firstname: string = "Angular tutorial";
+  angularVersion = "Version 19";
+  version : number = 19;
+  isActive : boolean = false;
+  currentDate : Date = new Date();
+  inputType: string = "radio";
+  selectedState : string = '';
 }
