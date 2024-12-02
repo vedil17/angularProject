b77a491b68c524e5526075b41d9839674ef2b553
adil alouan <62723653+vedil17@users.noreply.github.com>
2024-12-02 11:33:38 +0100

    init angular project

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles',
+  imports: [],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+export class RolesComponent {
+
+}
