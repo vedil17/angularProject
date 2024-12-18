13c3970fb9164479f7061d5ce55ecbffcef9125a
adil alouan <62723653+vedil17@users.noreply.github.com>
2024-12-18 05:26:14 +0100

    Routing and change the port

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -12,10 +12,12 @@ export class DesignationComponent implements OnInit{
 
   masterService = inject(MasterService)
   designationList: IDesignation [] = [];
+  isLoading: boolean = true;
   
   ngOnInit(): void {
     this.masterService.getDesignation().subscribe((results: APIResponseModel) => {
       this.designationList = results.data;
+      this.isLoading = false;
     }, error => {
       alert("API Error, Network down");
     })
